098e958baac929063f8c5bc13d7d55c82ed7e6b1
Hem Raj Pandey <hemrajpandeyd@gmail.com>
2023-11-10 18:12:12 +0545

    made responsive

--- src/Relief.js
+++ src/Relief.js
@@ -6,11 +6,12 @@ import { Link } from 'react-router-dom';
 function Relief() {
   return (
     <div className="relief-container">
-      <h1>Request for Relief Assistance</h1>
+    
+      <h1>  <i class="fas fa-hands-helping"></i> Request for Relief Assistance</h1>
       <div className='TextImg'>
         <div className="description">
           <p>
-            If you have recently experienced a disaster, we are here to assist you. Please fill out the form and click "Proceed" to submit your information. Provide details about your situation and attach images, helping us better understand the impact on your location or home.
+            If you have recently experienced a disaster, we are here to assist you. Please fill out the form by clicking on "Proceed" button and submit your information. Provide details about your situation and attach images, helping us better understand the impact on your location or home.
           </p>
           <p>
             Once we receive your request, our team will promptly initiate the necessary relief efforts. After submission, the required assistance will be provided by your local municipality or ward.
