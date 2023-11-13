6d4b9077684964f4d18eac5eaa544fa759614bcc
Hem Raj Pandey <hemrajpandeyd@gmail.com>
2023-11-13 10:17:59 +0545

    post request can be done using this form

--- src/QuickAid.js
+++ src/QuickAid.js
@@ -14,8 +14,10 @@ const QuickAid = () => {
   const [img, setImg] = useState(null);
   const [palika, setPalika] = useState('');
   const [ward, setWard] = useState('');
-  const [coordinate, setCoordinates] = useState('');
-
+  const [coordinate, setCoordinate] = useState('');
+  console.log(palika)
+  console.log(disaster)
+  console.log(location)
   const nextPage = () => {
     setCurrentPage((prevPage) => prevPage + 1);
   };
@@ -26,15 +28,18 @@ const QuickAid = () => {
 
   const getCurrentLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        const LC = `${latitude},${longitude}`;
-        setLocation(LC);
-        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-      }, (error) => {
-        console.error(`Geolocation error: ${error.message}`);
-      });
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const LC = `${latitude},${longitude}`;
+          setLocation(LC);
+          console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        },
+        (error) => {
+          console.error(`Geolocation error: ${error.message}`);
+        }
+      );
     } else {
       console.error('Geolocation is not supported by this browser.');
     }
@@ -45,64 +50,30 @@ const QuickAid = () => {
     setImg(file);
   };
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handlePhoneChange = (e) => {
-    setPhone(e.target.value);
-  };
-
-  const handleCitizenship_noChange = (e) => {
-    setCitizenship_no(e.target.value);
-  };
-
-  const handleHouse_noChange = (e) => {
-    setHouse_no(e.target.value);
-  };
-
-  const handleDisasterChange = (e) => {
-    setDisaster(e.target.value);
-  };
-
-  const handlePalikaChange = (e) => {
-    setPalika(e.target.value);
-    console.log(palika)
-  };
-
-  const handleWardChange = (e) => {
-    setWard(e.target.value);
-  };
-
-  const handleCoordinateChange = (e) => {
-    setCoordinates(e.target.value);
-  };
-
   const handleSubmit = async () => {
     try {
       const formData = new FormData();
-      formData.append('palika', palika); // Use the state value directly
+      formData.append('palika', palika);
       formData.append('ward', ward);
-      formData.append('location', coordinate);
+      formData.append('location', location);
       formData.append('name', name);
       formData.append('phone', phone);
       formData.append('citizenship_no', citizenship_no);
       formData.append('House_no', house_no);
-      formData.append('disaster', disaster); // Use the state value directly
+      formData.append('disaster', disaster);
       formData.append('img', img);
-  
+
       console.log(formData);
-  
+
       const response = await axios.post('http://127.0.0.1:2500/relief_request', formData);
-  
+
       console.log('Response from the server:', response.data);
-  
+
       // You can also redirect to a success page or perform other actions based on the response
     } catch (error) {
       console.error('Error sending request:', error.message);
     }
   };
-  
 
   return (
     <div className="quick-aid-form-unique">
@@ -111,26 +82,31 @@ const QuickAid = () => {
           <h1 className="quick-aid-heading">राहत सहयोगको लागि अनुरोध- पृष्ठ 1</h1>
           <div className="form-section-unique">
             <label htmlFor="name-unique">नाम (Name)*</label>
-            <input type="text" id="name-unique" value={name} onChange={handleNameChange} />
+            <input type="text" id="name-unique" value={name} onChange={(e) => setName(e.target.value)} />
           </div>
           <div className="form-section-unique">
             <label htmlFor="phone-unique"> फोन (Phone) *</label>
-            <input type="tel" id="phone-unique" value={phone} onChange={handlePhoneChange} />
+            <input type="tel" id="phone-unique" value={phone} onChange={(e) => setPhone(e.target.value)} />
           </div>
           <div className="form-section-unique">
             <label htmlFor="citizenship-unique">नागरिकता नं (Citizenship No ) *</label>
-            <input type="text" id="citizenship-unique" value={citizenship_no} onChange={handleCitizenship_noChange} />
+            <input type="text" id="citizenship-unique" value={citizenship_no} onChange={(e) => setCitizenship_no(e.target.value)} />
           </div>
           <div className="form-section-unique">
             <label htmlFor="house-unique">घर नं (House No)*</label>
-            <input type="text" id="house-unique" value={house_no} onChange={handleHouse_noChange} />
+            <input type="text" id="house-unique" value={house_no} onChange={(e) => setHouse_no(e.target.value)} />
           </div>
           <div className="form-section-unique">
-            <label htmlFor="reliefFrom-unique">सहायता बाट (Relief From Disaster) *</label>
-            <select id="reliefFrom-unique" value={disaster} onChange={handleDisasterChange}>
-              <option value="earthquake">भूकम्प (Earthquake ) </option>
-              {/* ... Other options ... (अन्य विकल्पहरू) */}
-            </select>
+            <label htmlFor="reliefFrom-unique">प्रकोप चयन गर्नुहोस् *</label>
+            <select id="reliefFrom-unique" value={disaster} onChange={(e) => setDisaster(e.target.value)}>
+  <option value="">Select a Disaster</option>
+  <option value="earthquake">भूकम्प (Earthquake)</option>
+  <option value="flood">बाढी (Flood)</option>
+  <option value="landslide">पहिरो (Landslide)</option>
+  <option value="fire">आगो (Fire)</option>
+  {/* ... Add more disaster options as needed ... */}
+</select>
+
           </div>
           <div className="form-section-unique">
             <label htmlFor="file-unique">तस्वीर अपलोड गर्नुहोस् (Upload Image) *</label>
@@ -149,32 +125,32 @@ const QuickAid = () => {
         <div className="column-unique column-right-unique">
           <h1 className="quick-aid-heading">राहत सहयोगको लागि अनुरोध- पृष्ठ 2 </h1>
           <div className="form-section-unique">
-            <label htmlFor="municipality-unique">पालिका चयन गर्नुहोस् (Select a palika) *</label>
-            <select id="municipality-unique" value={palika} onChange={handlePalikaChange}>
-        
-              <option value="Bungal">बुङ्गल नगरपालिका</option>
-              <option value="Bitthadchir">बित्थडचिर गाँउपालिका</option>
-              <option value="Chhabispathibhera">छबिसपाथिभेरा गाँउपालिका</option>
-              <option value="Durgathali">दुर्गाथली गाँउपालिका</option>
-              <option value="Jayaprithvi">जयपृथ्वी नगरपालिका</option>
-              <option value="Saipal">सइपाल गाउपालिका</option>
-              <option value="Kedarsyun">केदारस्युँ गाँउपालिका</option>
-              <option value="Khaptadchhanna">खप्तडछान्ना गाउँपालिका</option>
-              <option value="Mastha">मष्टा गाउँपालिका</option>
-              <option value="Surma">सूर्मा गाउँपालिका</option>
-              <option value="Talakot">तलकोट गाँउपालिका</option>
-              <option value="Thalara">थलारा गाउँपालिका</option>
-      
-            </select>
+            <label htmlFor="municipality-unique">पालिका चयन गर्नुहोस् *</label>
+            <select id="municipality-unique" value={palika} onChange={(e) => setPalika(e.target.value)}>
+  <option value="">Select a Palika</option>
+  <option value="Bungal">बुङ्गल नगरपालिका</option>
+  <option value="Bitthadchir">बित्थडचिर गाँउपालिका</option>
+  <option value="Chhabispathibhera">छबिसपाथिभेरा गाँउपालिका</option>
+  <option value="Durgathali">दुर्गाथली गाँउपालिका</option>
+  <option value="Jayaprithvi">जयपृथ्वी नगरपालिका</option>
+  <option value="Saipal">सइपाल गाउपालिका</option>
+  <option value="Kedarsyun">केदारस्युँ गाँउपालिका</option>
+  <option value="Khaptadchhanna">खप्तडछान्ना गाउँपालिका</option>
+  <option value="Mastha">मष्टा गाउँपालिका</option>
+  <option value="Surma">सूर्मा गाउँपालिका</option>
+  <option value="Talakot">तलकोट गाँउपालिका</option>
+  <option value="Thalara">थलारा गाउँपालिका</option>
+</select>
+
           </div>
           <div className="form-section-unique">
             <label htmlFor="ward-unique">Ward (वार्ड) *</label>
-            <input type="text" id="ward-unique" value={ward} onChange={handleWardChange} />
+            <input type="text" id="ward-unique" value={ward} onChange={(e) => setWard(e.target.value)} />
           </div>
           <div className="form-section-unique">
             <label htmlFor="location-unique">Location (स्थान) *</label>
             <div className="location-input-wrapper">
-              <input type="text" id="location-unique" value={location} onChange={handleCoordinateChange} />
+              <input type="text" id="location-unique" value={location} onChange={(e) => setCoordinate(e.target.value)} />
               <button type="button" className="location-icon" onClick={getCurrentLocation}>
                 <i className="fas fa-map-marker-alt"></i>
               </button>
