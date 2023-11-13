091206e2876c7bb4d17d5fbe1570ec5f2e769130
Hem Raj Pandey <hemrajpandeyd@gmail.com>
2023-11-13 15:44:59 +0545

    made responsive palika page

--- src/PalikaDashboard.js
+++ src/PalikaDashboard.js
@@ -7,20 +7,33 @@ function PalikaDashboard() {
       {/* Palika Name */}
       <h1 className="palika-name">Surma Gaupalika Admin Panel</h1>
 
-      {/* Relief Request Section */}
-      <div className="section">
-        <div className="section-item">
-          <h3><i className="fas fa-hands-helping"></i> Relief Requests</h3>
-          {/* Add your content for the Relief Request section here */}
-        </div>
-      </div>
+      {/* Sections */}
+      <div className='sections'>
 
-      {/* Data Upload Section */}
-      <div className="section">
-        <div className="section-item">
-          <h3><i className="fas fa-upload"></i> Data Upload</h3>
-          {/* Add your content for the Data Upload section here */}
+        {/* Relief Request Section */}
+        <div className="section">
+          <div className="section-item">
+            <h3><i className="fas fa-hands-helping"></i> Relief Requests</h3>
+            {/* Add your content for the Relief Request section here */}
+          </div>
         </div>
+
+        {/* Add Place Section */}
+        <div className="section">
+          <div className="section-item">
+            <h3><i className="fas fa-map-marker-alt"></i> Add Place</h3>
+            {/* Add your content for the Add Place section here */}
+          </div>
+        </div>
+
+        {/* Publish Layer Section */}
+        <div className="section">
+          <div className="section-item">
+            <h3><i className="fas fa-layer-group"></i> Publish Layer</h3>
+            {/* Add your content for the Publish Layer section here */}
+          </div>
+        </div>
+
       </div>
     </div>
   );
