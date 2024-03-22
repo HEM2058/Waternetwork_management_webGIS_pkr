ac990455196ef378e795cec4593dd4e0c046743e
Hem Raj Pandey <hemrajpandeyd@gmail.com>
2024-03-22 17:18:51 +0545

    added the api for task fetching

--- src/Task.js
+++ src/Task.js
@@ -1,14 +1,23 @@
-// Task.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Task.css';
+import axios from 'axios'; // Import Axios for making HTTP requests
 
 function Task() {
-  const [tasks, setTasks] = useState([
-    { id: 1, name: 'Pipeline Installation (Mahendrapool)', assignedTo: ['Ram', 'Hari'] },
-    { id: 2, name: 'Pipeline Installation (Lamachaur)', assignedTo: ['Laxman', 'Bharat'] },
-    { id: 3, name: 'Pipeline Installation (Batulechaur)', assignedTo: ['Laxman', 'Bharat'] },
-    { id: 3, name: 'Pipeline Installation (Batulechaur)', assignedTo: ['Laxman', 'Bharat'] },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Fetch tasks from the API when the component mounts
+    fetchTasks();
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/tasks/');
+      setTasks(response.data);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
 
   const handleAddTask = () => {
     // TODO: Implement logic to add a new task
@@ -17,19 +26,24 @@ function Task() {
 
   return (
     <div className="task-container">
-      <h2>Task Management</h2>
+      <h2>Task Manager</h2>
       <button className="add-task-button" onClick={handleAddTask}>
         <i className="fas fa-plus"></i> Add Task
       </button>
-     <div className='tasks'>
-      <div className="task-list">
-        {tasks.map((task) => (
-          <div key={task.id} className="task-item">
-            <h3>{task.name}</h3>
-            <p>Assigned to: {task.assignedTo.join(', ')}</p>
-          </div>
-          
-        ))}
+      <div className='tasks'>
+        <div className="task-list">
+          {tasks.map((task) => (
+            <div key={task.id} className="task-item">
+              <h3>{task.task_name}</h3>
+              <p>Description: {task.description}</p>
+              <p>Status: {task.status}</p>
+              <p>Assigned to: {task.assigned_to}</p>
+              <p>Start Date: {new Date(task.start_date).toLocaleDateString()}</p>
+              <p>Deadline: {new Date(task.deadline).toLocaleDateString()}</p>
+              <button className="view-more-button">Edit Task</button>
+              <button className="view-map-button">View on Map</button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
